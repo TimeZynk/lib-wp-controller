4e32ae1d1646a15582527553b7cf59a389d10569
Johan Heander <johan@timezynk.com>
2022-10-25 12:02:59 +0200

    Switch widgetId depending on language

--- timezynk-wp-controller.php
+++ timezynk-wp-controller.php
@@ -4,14 +4,14 @@
  * Text Domain: timezynk-wp-controller
  * Plugin URI: https://github.com/TimeZynk/timezynk-wp-controller
  * Description: Links in signup and pricing scripts
- * Version: 1.7.1
+ * Version: 1.7.2
  * Author: Timezynk Dev
  * Author URI: https://timezynk.com
  * GitHub Plugin URI: https://github.com/TimeZynk/timezynk-wp-controller
  */
 
 function timezynk_scripts_enqueue_scripts() {
-    $script_version = '1.7.1';
+    $script_version = '1.7.2';
     wp_enqueue_script('timezynk_password_strength', plugins_url( 'js/password-strength.js', __FILE__ ), array(), $script_version, true);
     wp_enqueue_script('timezynk_signup', plugins_url( 'js/signup.js', __FILE__), array(), $script_version, true);
     wp_localize_script('timezynk_password_strength', 'password_messages', array(
@@ -24,11 +24,13 @@ function timezynk_scripts_enqueue_scripts() {
 }
 
 function timezynk_scripts_nativeforms_snippet() {
+    $my_current_lang = apply_filters( 'wpml_current_language', NULL );
+    $widgetId = $my_current_lang == "sv" ? 'b7i4' : '1';
     ?>
         <script>
         window.nativeForms = {
-        license: "6nLEzw",
-        widgetId: "1"
+            license: "6nLEzw",
+            widgetId: "<?php echo $widgetId ?>"
         };
         </script>
         <script src="https://script.nativeforms.com/main.js" async></script>
